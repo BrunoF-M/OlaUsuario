beaae3416c5b9dc2afca4342fc28008ebd1e7976
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-12 02:39:26 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+
+            string nome;
+            Console.WriteLine("Olá qual é seu nome? ");
+            nome = Console.ReadLine();
+            Console.WriteLine($"Olá {nome}!");
+        }
+    }
+}
